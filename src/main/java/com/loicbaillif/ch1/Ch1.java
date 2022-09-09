78f8f690e01ffc9790d7c5970e2f49d3ddaeeb39
loicbaillif <larmedeciel@gmail.com>
2022-09-09 22:09:22 +0200

    oc/learnJava/ch1/init
    Chapter 1: Heritage and polymorphism
    Initialization

---
+++ src/main/java/com/loicbaillif/ch1/Ch1.java
@@ -0,0 +1,10 @@
+package com.loicbaillif.ch1;
+
+import com.loicbaillif.tools.Print;
+
+public class Ch1 {
+    public static void main() {
+        Print.subtitle("Ch1: Heritage and Polymorphism");
+    }
+
+}
