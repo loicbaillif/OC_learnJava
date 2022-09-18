74918de09853bf2ef1ea2179f809cccd675223b2
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-09-18 16:19:40 +0200

    oc/learnJava/ch2/v1
    First array declaration, and fill

--- src/main/java/com/loicbaillif/ch2/Ch2.java
+++ src/main/java/com/loicbaillif/ch2/Ch2.java
@@ -5,6 +5,13 @@ import com.loicbaillif.tools.Print;
 public class Ch2 {
     public static void main() {
         Print.subtitle("Ch2: Handle data stacks");
+
+        System.out.println("***** Arrays *****");
+        int[] cupsOfCoffeePerWeek; // From monday to sunday
+        cupsOfCoffeePerWeek = new int[7];
+        int[] daysPerMonth = new int[12];
+        daysPerMonth[1] = 28; // February
+        System.out.printf("Month n°2 has %d days", daysPerMonth[1]);
     }
 
 }
