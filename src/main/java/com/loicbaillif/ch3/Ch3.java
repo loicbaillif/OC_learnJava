bef8f21e1e58a3e2bb32758e078e10008dcde146
loicbaillif <larmedeciel@gmail.com>
2022-09-25 17:58:24 +0200

    oc/learnJava/ch3/v1
    First function with parameters, and usage

--- src/main/java/com/loicbaillif/ch3/Ch3.java
+++ src/main/java/com/loicbaillif/ch3/Ch3.java
@@ -6,8 +6,13 @@ public class Ch3 {
     public static void main() {
         Print.title("Ch3: Ways to pass parameters");
 
-        
+        displayPerimeter(3, 5);
 
         Print.subtitle("End of chapter 3");
+    }
+
+    public static void displayPerimeter(int length, int width) {
+        int perimeter = 2 * (length + width);
+        System.out.printf("Perimeter = %d%n", perimeter);
     }
 }
