e77a269a0880039a5227df10e733d2721dc1e490
loicbaillif <larmedeciel@gmail.com>
2022-09-25 11:04:14 +0200

    oc/learnJava/ch1/v3
    Titles re-organized for clarity
    End of chapter

--- src/main/java/com/loicbaillif/ch1/Ch1.java
+++ src/main/java/com/loicbaillif/ch1/Ch1.java
@@ -4,7 +4,7 @@ import com.loicbaillif.tools.Print;
 
 public class Ch1 {
     public static void main() {
-        Print.subtitle("Ch1: Heritage and Polymorphism", '#', true);
+        Print.title("Ch1: Heritage and Polymorphism");
         GeometricShape shape1 = new GeometricShape(2, 5);
         shape1.moveTo(3, -1);
         Square square1 = new Square(-3, -4, -2);
