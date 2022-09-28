41b9a818d0511174d85e5883cde08691b2e52134
loicbaillif <larmedeciel@gmail.com>
2022-09-28 20:47:57 +0200

    oc/learnJava/ch4/v1
    Factorial function: first example of recursive function

--- src/main/java/com/loicbaillif/ch4/Ch4.java
+++ src/main/java/com/loicbaillif/ch4/Ch4.java
@@ -6,7 +6,18 @@ public class Ch4 {
     public static void main() {
         Print.title("Ch4: Recursive functions");
 
+        // First example: calculate factorial
+        int[] integerArrays = {0, 1, 2, 3, 5, 8};
+        for (int element: integerArrays) {
+            System.out.printf(". %d! = %d%n", element, factorial(element));
+        }
 
         Print.subtitle("End of chapter 4");
+    }
+
+
+    public static int factorial(int n) {
+        if (n <= 1) return 1;
+        else return n * factorial(n - 1);
     }
 }
